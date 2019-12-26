8a70c6e47de1b5b8fd9bbdf07100dba4bc20818b
Erold Lopez <eroldlopez@HUAWEI_nova_3i-fa7313168b.localdomain>
2019-12-26 15:09:01 +0800

    Initial codes

---
+++ learning-main/main.py
@@ -0,0 +1,24 @@
+import tornado.ioloop
+import tornado.web
+import json
+import zmq
+
+class MainHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        response = "Hello World... Server responsed"
+        self.write(response)
+
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+
+    ])
+
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(5008)
+    tornado.ioloop.IOLoop.current().start()
+
